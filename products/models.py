a15a66b8990e2804016bafea0edb31a15a483774
Sven12345re <44273129+Sven12345re@users.noreply.github.com>
2020-07-03 16:44:01 +0200

    Stars

--- products/models.py
+++ products/models.py
@@ -76,6 +76,7 @@ class Comment(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    rate = models.FloatField(max_length=500)
 
     class Meta:
         ordering = ['timestamp']
